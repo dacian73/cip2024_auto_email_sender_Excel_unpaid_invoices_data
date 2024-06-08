f545fe8317502dd6c314bb36a3431c63fbcbb6bd
dacian73 <dacian73@gmail.com>
2024-06-08 21:05:44 +0300

    get names and emails from excel file... temporary commented out the code to send emails with outlook using win32

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from tkinter.filedialog import askopenfile
 import tkinter as tk
 import pandas as pd
 
+# The user interface
 class MyGui:
     def __init__(self, master):
+        emails = ""
+
+        def get_emails(event):
+            emails = emailsText.get("1.0",'end-1c')
+            print("TEEEEEEEEEEEEEEEEEEEEEST", emails, "SUPPPPPPPPPPPPPPPEEEEEEEEEEERRRRRRRR")
+        
         self.root = master
         self.root.geometry("600x600")
+
+        # Header
         header = tk.Label(self.root, text="Bulk email sender")
         header.pack(padx=20, pady=20)
 
@@ -16,8 +25,8 @@ class MyGui:
 
         namesText = tk.Text(xFrame)
         emailsText = tk.Text(xFrame)
-        sendButton = tk.Button(self.root, text="Open Excel file", command=lambda:self.chooseFile(namesText, emailsText))
-        sendButton.pack()
+        openFileButton = tk.Button(self.root, text="Open Excel file", command=lambda:self.chooseFile(namesText, emailsText))
+        openFileButton.pack()
 
      
 
@@ -25,28 +34,56 @@ class MyGui:
         emailsText.grid(row=0, column =1, sticky= tk.W+tk.E)
         xFrame.pack()
 
+        emailsText.bind('<KeyPress>', get_emails)
+
+        sendEmailsButton = tk.Button(self.root, text="Send Emails", command=lambda:self.send_emails(emails, subjects, bodies))
+        sendEmailsButton.pack()
 
 
+        
+
+    # Let the user upload an Excel file with columns for name, email
     def chooseFile(self, names_text_widget, emails_text_widget): 
         filename = askopenfile()
         print("We are attempting to read the file: \n", filename)
+        print()
+
         
         email_list = pd.read_excel(filename.name)
+        print("Email list = ", email_list)
+
         names = email_list['name']
+        print("The names we identified in the file are:\n", names)
+        print()
         change_text(names_text_widget, names)
         
         emails = email_list['email']
+        print("The emails we identified are:\n", emails)
+        print()
         change_text(emails_text_widget, emails)
-#     print("The names we identified in the file are:\n", names)
-#     print()
-#     print("The emails we identified are:\n", emails)
-#     print()
+
+    def send_emails(emails, subjects, bodies):
+        outlook = win32.Dispatch('Outlook.Application')
+ 
+
+        for i in range(len(emails)):
+ 
+    # for every record create an email
+            mail = outlook.CreateItem(0)
+            mail.To = emails[i]
+            mail.Subject = subjects[i]
+            mail.Body = bodies[i]
+ 
+    # sending the email
+            mail.Send()
 
 def change_text(text_widget, new_text):
-    # Delete the current content from the beginning ('1.0') to the end ('end')
+    # Delete the current content for the text widget
     text_widget.delete('1.0', 'end')
-    # Insert the new text at the beginning ('1.0')
+    # Insert the new text for the text widget
     text_widget.insert('1.0', new_text)
+
+
 
 def main():
     root = tk.Tk()
@@ -55,51 +92,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-   
-   
-# def main():
-
-
-#     B = Button(top, text ="Open Excel file", command = choose_file())
-#     B.place(x=50,y=50)
-
-#     print(B.command)
-    
-#     # send_emails(filename)
-
-# # close the smtp server
-#     print("Test run concluded")
-
-# def choose_file():
-#     filename = askopenfile()
-#     print("We are attempting to read the file: \n", filename)
-#     return filename
-
-# def send_emails(filename):
-#     outlook = win32.Dispatch('Outlook.Application')
-#     email_list = pd.read_excel(filename.name)
-    
-#     names = email_list['name']
-#     emails = email_list['email']
-
-#     print("The names we identified in the file are:\n", names)
-#     print()
-#     print("The emails we identified are:\n", emails)
-#     print()
- 
-
-#     for i in range(len(emails)):
- 
-#     # for every record get the name and the email addresses
-#         mail = outlook.CreateItem(0)
-#         mail.To = emails[i]
-#         mail.Subject = 'Do not forget you have an unpaid invoice, dear ' + names[i]
-#         mail.Body = 'MRAndom text 2, yey'
- 
-#     # # sending the email
-#     mail.Send()
-
-
-# if __name__ == '__main__':
-#     main()
