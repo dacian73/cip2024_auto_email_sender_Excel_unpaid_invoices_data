077b5c501e84cddf3467b6afa865945ac0846034
dacian73 <dacian73@gmail.com>
2024-06-10 11:52:34 +0300

    added the option to edit the email body template, (not yet working)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 emails = []
 names = []
+global body_template 
+body_template = ["Hello, ", "$name", "\n", "We want to inform you that you have an unpaid invoice with our company. You can find the details of outstanding payment bellow:"]
 # The user interface
 class MyGui:
     def __init__(self, master):
@@ -13,33 +15,49 @@ class MyGui:
         self.root.geometry("600x600")
 
         # Header
-        header = tk.Label(self.root, text="Bulk email sender")
+        header = tk.Label(self.root, text="Bulk email sender", font=("Arial", 18))
         header.pack(padx=20, pady=20)
+
+        templateLabel = tk.Label(self.root, text="Email Template", font=("Arial", 12))
+        templateLabel.pack()
+
+        editTemplateLabel = tk.Label(self.root, text="You can edit the template bellow. you need to write $name instead of the client name.", font=("Arial", 8))
+        editTemplateLabel.pack()
+
+        templateText = tk.Text(self.root, font=("Arial", 10), height=3)
+        displayable_body_template = ""
+        for sequence in body_template:
+                if sequence == "$name":
+                    displayable_body_template = displayable_body_template + "$name"
+                else:
+                    displayable_body_template = displayable_body_template+sequence
+
+        # TODO, change the body template when the text in the templateText widget changes
+
+        templateText.insert("end-1c", displayable_body_template)
+        templateText.pack(padx=10, pady=10)
+        templateText.bind('<KeyPress>', update_body_template(templateText))
 
         xFrame = tk.Frame(self.root)
         xFrame.columnconfigure(0, weight=1)
         xFrame.columnconfigure(1,weight=1)
 
-        namesList = tk.Listbox(xFrame)
-        emailsList = tk.Listbox(xFrame)
-        openFileButton = tk.Button(self.root, text="Open Excel file", command=lambda:self.chooseFile(namesList, emailsList))
+        namesListBox = tk.Listbox(xFrame)
+        emailsListBox = tk.Listbox(xFrame)
+        openFileButton = tk.Button(self.root, text="Open Excel file", command=lambda:self.chooseFile(namesListBox, emailsListBox))
         openFileButton.pack()
 
-        namesList.grid(row=0, column =0, sticky= tk.W+tk.E)
-        emailsList.grid(row=0, column =1, sticky= tk.W+tk.E)
+        namesListBox.grid(row=0, column =0, sticky= tk.W+tk.E)
+        emailsListBox.grid(row=0, column =1, sticky= tk.W+tk.E)
         xFrame.pack()
-
-        subjects = ["subject1","subject2","subject3"]
-        bodies = ["f","s","a"]
-
-        for i in range(len(emails)):
-            bodies[i] = names[i] + " ai o factură neplătită"
 
         sendEmailsButton = tk.Button(self.root, text="Send Emails", command=self.on_send_emails_button_click)
         sendEmailsButton.pack()
+    
+
 
     # Let the user upload an Excel file with columns for name, email
-    def chooseFile(self, names_list_widget, emails_list_widget): 
+    def chooseFile(self, names_listbox, emails_listbox): 
         filename = askopenfile()
         print("We are attempting to read the file: \n", filename)
         print()
@@ -52,13 +70,13 @@ class MyGui:
         names = email_list['name'].to_list()
         print("The names we identified in the file are:\n", names)
         print()
-        change_text(names_list_widget, names)
+        change_text(names_listbox, names)
         
         global emails
         emails = email_list['email'].to_list()
         print("The emails we identified are:\n", emails)
         print()
-        change_text(emails_list_widget, emails)
+        change_text(emails_listbox, emails)
 
         
 
@@ -83,10 +101,22 @@ class MyGui:
            # mail.Send()
     
     def on_send_emails_button_click(self):
-        subjects = ["subject1", "subject2", "subject3"]
-        bodies = ["f", "s", "a"]
+        # Create lists for email subjects and bodies with the same number of elements as the emails list
+        subjects = ["Unpaid Invoice Notification" for i in range(len(emails))]
+        bodies = ["" for i in range(len(emails))]
+
+        # Populate the lists with subjects and bodies as needed
         for i, name in enumerate(names):
-            bodies[i] = name + " ai o factură neplătită"
+
+            # Create an email body using the template and replacing the name where needed
+            body = ""
+            for sequence in body_template:
+                if sequence == "$name ":
+                    body = body + name
+                else:
+                    body = body+sequence
+            
+            bodies[i] = body
         self.send_emails(subjects, bodies)
 
 def change_text(text_widget, new_text):
@@ -95,7 +125,14 @@ def change_text(text_widget, new_text):
     # Insert the new text for the text widget
     for word in new_text:
         text_widget.insert('0', word)
-
+        
+def update_body_template(text_widget):
+    global body_template
+    body_template = [""]
+    print(text_widget.get("1.0",'end-1c'))
+    for sequence in text_widget.get("1.0",'end-1c').split():
+        body_template.append(sequence + " ")
+    print("AICI", body_template)
 
 
 def main():
